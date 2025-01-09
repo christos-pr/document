7097c218b1d8f91fdcd6608bc343fd0d94b14f7c
christos-pr <113031067+christos-pr@users.noreply.github.com>
2025-01-09 18:16:12 +0200

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,325 @@
+/**
+ * Document Tool for the Editor.js
+ *
+ * @author CodeX <team@codex.so>
+ * @license MIT
+ * @see {@link https://github.com/editor-js/image}
+ *
+ * To developers.
+ * To simplify Tool structure, we split it to 4 parts:
+ *  1) index.js — main Tool's interface, public API and methods for working with data
+ *  2) uploader.js — module that has methods for sending files via AJAX: from media gallery component
+ *  3) ui.js — module for UI manipulations: render, showing preloader, etc
+ *  4) tunes.js — working with Block Tunes: render buttons, handle clicks
+ *
+ * For debug purposes there is a testing server
+ * that can save uploaded files and return a Response {@link UploadResponseFormat}
+ *
+ *       $ node dev/server.js
+ *
+ * It will expose 8008 port, so you can pass http://localhost:8008 with the Tools config:
+ *
+ * document: {
+ *   class: DocumentTool,
+ *   config: {
+ *     selectFiles: () => { }
+ *   },
+ * },
+ */
+
+/**
+ * @typedef {object} DocumentToolData
+ * @description Document Tool's input and output data format
+ * @property {string} caption — Document caption
+ * @property {object} file — Document file data returned from backend
+ * @property {string} file.url — Document URL
+ */
+
+import './index.css';
+
+import Ui from './ui';
+import Uploader from './uploader';
+
+/**
+ * @typedef {object} DocumentConfig
+ * @description Config supported by Tool
+ * @property {object} endpoints - upload endpoints
+ * @property {string} endpoints.byFile - upload by file
+ * @property {string} endpoints.byUrl - upload by URL
+ * @property {string} field - field name for uploaded document
+ * @property {string} types - available mime-types
+ * @property {string} captionPlaceholder - placeholder for Caption field
+ * @property {object} additionalRequestData - any data to send with requests
+ * @property {object} additionalRequestHeaders - allows to pass custom headers with Request
+ * @property {string} buttonContent - overrides for Select File button
+ * @property {object} [uploader] - optional custom uploader
+ * @property {function(): Promise.<UploadResponseFormat>} selectFiles - method that selects an document file from a custom file/ asset manager
+ */
+
+/**
+ * @typedef {object} UploadResponseFormat
+ * @description This format expected from backend on file uploading
+ * @property {number} success - 1 for successful uploading, 0 for failure
+ * @property {object} file - Object with file data.
+ *                           'url' is required,
+ *                           also can contain any additional data that will be saved and passed back
+ * @property {string} file.url - [Required] document source URL
+ */
+export default class DocumentTool {
+  /**
+   * Notify core that read-only mode is supported
+   *
+   * @returns {boolean}
+   */
+  static get isReadOnlySupported() {
+    return true;
+  }
+
+  /**
+   * Get Tool toolbox settings
+   * icon - Tool icon's SVG
+   * title - title to show in toolbox
+   *
+   * @returns {{icon: string, title: string}}
+   */
+  static get toolbox() {
+    return {
+      title: 'Document',
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#5c6b7a" viewBox="0 0 256 256"><path d="M80,224a8,8,0,0,1-8,8H56a16,16,0,0,1-16-16V184a8,8,0,0,1,16,0v32H72A8,8,0,0,1,80,224ZM213.66,82.34l-56-56A8,8,0,0,0,152,24H120a8,8,0,0,0,0,16h24V88a8,8,0,0,0,8,8h48v40a8,8,0,0,0,16,0V88A8,8,0,0,0,213.66,82.34ZM80,24H56A16,16,0,0,0,40,40V64a8,8,0,0,0,16,0V40H80a8,8,0,0,0,0-16ZM208,168a8,8,0,0,0-8,8v40h-8a8,8,0,0,0,0,16h8a16,16,0,0,0,16-16V176A8,8,0,0,0,208,168ZM48,152a8,8,0,0,0,8-8V104a8,8,0,0,0-16,0v40A8,8,0,0,0,48,152Zm104,64H112a8,8,0,0,0,0,16h40a8,8,0,0,0,0-16Z"></path></svg>`,
+    };
+  }
+
+  /**
+   * Available document tools
+   *
+   * @returns {Array}
+   */
+  static get tunes() {
+    return [
+    ];
+  }
+
+  /**
+   * @param {object} tool - tool properties got from editor.js
+   * @param {DocumentToolData} tool.data - previously saved data
+   * @param {DocumentConfig} tool.config - user config for Tool
+   * @param {object} tool.api - Editor.js API
+   * @param {boolean} tool.readOnly - read-only mode flag
+   */
+  constructor({ data, config, api, readOnly }) {
+    this.api = api;
+    this.readOnly = readOnly;
+
+    /**
+     * Tool's initial config
+     */
+    this.config = {
+      endpoints: config.endpoints || '',
+      additionalRequestData: config.additionalRequestData || {},
+      additionalRequestHeaders: config.additionalRequestHeaders || {},
+      field: config.field || 'document',
+      types: config.types || 'document/*',
+      captionPlaceholder: this.api.i18n.t(config.captionPlaceholder || 'Caption'),
+      buttonContent: config.buttonContent || '',
+      uploader: config.uploader || undefined,
+      actions: config.actions || [],
+      selectFiles: config.selectFiles || undefined,
+    };
+
+    /**
+     * Module for file uploading
+     */
+    this.uploader = new Uploader({
+      config: this.config
+    });
+
+    /**
+     * Module for working with UI
+     */
+    this.ui = new Ui({
+      api,
+      config: this.config,
+      onSelectFile: () => {
+        this.uploader.uploadSelectedFile({
+          onPreview: (src) => {
+            this.ui.showPreloader(src);
+          },
+        });
+      },
+      readOnly,
+    });
+
+    /**
+     * Set saved state
+     */
+    this._data = {};
+    this.data = data;
+  }
+
+  /**
+   * Renders Block content
+   *
+   * @public
+   *
+   * @returns {HTMLDivElement}
+   */
+  render() {
+    return this.ui.render(this.data);
+  }
+
+  /**
+   * Validate data: check if sound file exists
+   *
+   * @param {DocumentToolData} savedData — data received after saving
+   * @returns {boolean} false if saved data is not correct, otherwise true
+   * @public
+   */
+  validate(savedData) {
+    return savedData.file && savedData.file.url;
+  }
+
+  /**
+   * Return Block data
+   *
+   * @public
+   *
+   * @returns {DocumentToolData}
+   */
+  save() {
+    const caption = this.ui.nodes.caption;
+
+    this._data.caption = caption.innerHTML;
+
+    return this.data;
+  }
+
+  /**
+   * Returns configuration for block tunes: add background, stretch document player
+   *
+   * @public
+   *
+   * @returns {Array}
+   */
+  renderSettings() {
+    // Merge default tunes with the ones that might be added by user
+    // @see https://github.com/editor-js/image/pull/49
+    const tunes = DocumentTool.tunes.concat(this.config.actions);
+
+    return tunes.map(tune => ({
+      icon: tune.icon,
+      label: this.api.i18n.t(tune.title),
+      name: tune.name,
+      toggle: tune.toggle,
+      isActive: this.data[tune.name],
+      onActivate: () => {
+        /* If it'a user defined tune, execute it's callback stored in action property */
+        if (typeof tune.action === 'function') {
+          tune.action(tune.name);
+
+          return;
+        }
+        this.tuneToggled(tune.name);
+      },
+    }));
+  }
+
+  /**
+   * Fires after clicks on the Toolbox Document Icon
+   * Initiates click on the Select File button
+   *
+   * @public
+   */
+  appendCallback() {
+    this.ui.nodes.fileButton.click();
+  }
+
+  /**
+   * Private methods
+   * ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿
+   */
+
+  /**
+   * Stores all Tool's data
+   *
+   * @private
+   *
+   * @param {DocumentToolData} data - data in Document Tool format
+   */
+  set data(data) {
+    this.document = data.file;
+
+    this._data.caption = data.caption || '';
+    this.ui.fillCaption(this._data.caption);
+
+    DocumentTool.tunes.forEach(({ name: tune }) => {
+      const value = typeof data[tune] !== 'undefined' ? data[tune] === true || data[tune] === 'true' : false;
+
+      this.setTune(tune, value);
+    });
+  }
+
+  /**
+   * Return Tool data
+   *
+   * @private
+   *
+   * @returns {DocumentToolData}
+   */
+  get data() {
+    return this._data;
+  }
+
+  /**
+   * Set new document file
+   *
+   * @private
+   *
+   * @param {object} file - uploaded file data
+   */
+  set document(file) {
+    this._data.file = file || {};
+    if (file && file.url) {
+      this.ui.fillDocument(file);
+    }
+  }
+
+  /**
+   * Callback fired when Block Tune is activated
+   *
+   * @private
+   *
+   * @param {string} tuneName - tune that has been clicked
+   * @returns {void}
+   */
+  tuneToggled(tuneName) {
+    // inverse tune state
+    this.setTune(tuneName, !this._data[tuneName]);
+  }
+
+  /**
+   * Set one tune
+   *
+   * @param {string} tuneName - {@link Tunes.tunes}
+   * @param {boolean} value - tune state
+   * @returns {void}
+   */
+  setTune(tuneName, value) {
+    this._data[tuneName] = value;
+
+    this.ui.applyTune(tuneName, value);
+
+    if (tuneName === 'stretched') {
+      /**
+       * Wait until the API is ready
+       */
+      Promise.resolve().then(() => {
+        const blockId = this.api.blocks.getCurrentBlockIndex();
+
+        this.api.blocks.stretchBlock(blockId, value);
+      })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
+}
---
+++ src/ui.js
@@ -0,0 +1,220 @@
+import { make } from './utils/dom';
+
+/**
+ * Class for working with UI:
+ *  - rendering base structure
+ *  - show/hide preview
+ *  - apply tune view
+ */
+export default class Ui {
+  /**
+   * @param {object} ui - document tool Ui module
+   * @param {object} ui.api - Editor.js API
+   * @param {DocumentConfig} ui.config - user config
+   * @param {Function} ui.onSelectFile - callback for clicks on Select file button
+   * @param {boolean} ui.readOnly - read-only mode flag
+   */
+  constructor({ api, config, onSelectFile, readOnly }) {
+    this.api = api;
+    this.config = config;
+    this.onSelectFile = onSelectFile;
+    this.readOnly = readOnly;
+    this.nodes = {
+      wrapper: make('div', [this.CSS.baseClass, this.CSS.wrapper]),
+      documentContainer: make('div', [ this.CSS.documentContainer ]),
+      fileButton: this.createFileButton(),
+      documentTitle: make('span', this.CSS.documentTitle),
+      documentElement: undefined,
+      // documentElement: make('div', this.CSS.documentElement),
+      documentPreloader: make('div', this.CSS.documentPreloader),
+      caption: make('div', [this.CSS.input, this.CSS.caption], {
+        contentEditable: !this.readOnly,
+      }),
+    };
+
+    /**
+     * Create base structure
+     *  <wrapper>
+     *    <document-container>
+     *      <document-preloader />
+     *    </document-container>
+     *    <caption />
+     *    <select-file-button />
+     *  </wrapper>
+     */
+    this.nodes.caption.dataset.placeholder = this.config.captionPlaceholder;
+    this.nodes.documentContainer.appendChild(this.nodes.documentPreloader);
+    this.nodes.wrapper.appendChild(this.nodes.documentContainer);
+    this.nodes.wrapper.appendChild(this.nodes.caption);
+    this.nodes.wrapper.appendChild(this.nodes.fileButton);
+  }
+
+  /**
+   * CSS classes
+   *
+   * @returns {object}
+   */
+  get CSS() {
+    return {
+      baseClass: this.api.styles.block,
+      loading: this.api.styles.loader,
+      input: this.api.styles.input,
+      button: this.api.styles.button,
+
+      /**
+       * Tool's classes
+       */
+      wrapper: 'document-tool',
+      documentContainer: 'document-tool__document',
+      documentPreloader: 'document-tool__document-preloader',
+      documentTitle: 'document-tool__document-title',
+      documentElement: 'document-tool__document-element',
+      caption: 'document-tool__caption',
+    };
+  };
+
+  /**
+   * Ui statuses:
+   * - empty
+   * - uploading
+   * - filled
+   *
+   * @returns {{EMPTY: string, UPLOADING: string, FILLED: string}}
+   */
+  static get status() {
+    return {
+      EMPTY: 'empty',
+      UPLOADING: 'loading',
+      FILLED: 'filled',
+    };
+  }
+
+  /**
+   * Renders tool UI
+   *
+   * @param {DocumenteToolData} toolData - saved tool data
+   * @returns {Element}
+   */
+  render(toolData) {
+    if (!toolData.file || Object.keys(toolData.file).length === 0) {
+      this.toggleStatus(Ui.status.EMPTY);
+    } else {
+      this.toggleStatus(Ui.status.UPLOADING);
+    }
+
+    return this.nodes.wrapper;
+  }
+
+  /**
+   * Creates upload-file button
+   *
+   * @returns {Element}
+   */
+  createFileButton() {
+    const button = make('div', [ this.CSS.button ]);
+
+    button.innerHTML = this.config.buttonContent || `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#5c6b7a" viewBox="0 0 256 256"><path d="M80,224a8,8,0,0,1-8,8H56a16,16,0,0,1-16-16V184a8,8,0,0,1,16,0v32H72A8,8,0,0,1,80,224ZM213.66,82.34l-56-56A8,8,0,0,0,152,24H120a8,8,0,0,0,0,16h24V88a8,8,0,0,0,8,8h48v40a8,8,0,0,0,16,0V88A8,8,0,0,0,213.66,82.34ZM80,24H56A16,16,0,0,0,40,40V64a8,8,0,0,0,16,0V40H80a8,8,0,0,0,0-16ZM208,168a8,8,0,0,0-8,8v40h-8a8,8,0,0,0,0,16h8a16,16,0,0,0,16-16V176A8,8,0,0,0,208,168ZM48,152a8,8,0,0,0,8-8V104a8,8,0,0,0-16,0v40A8,8,0,0,0,48,152Zm104,64H112a8,8,0,0,0,0,16h40a8,8,0,0,0,0-16Z"></path></svg> ${this.api.i18n.t('Click to select a document file...')}`;
+
+    button.addEventListener('click', () => {
+      this.onSelectFile();
+    });
+
+    return button;
+  }
+
+  /**
+   * Shows uploading preloader
+   *
+   * @param {string} src - preview source
+   * @returns {void}
+   */
+  showPreloader(src) {
+    this.nodes.documentPreloader.style.backgroundImage = `url(${src})`;
+
+    this.toggleStatus(Ui.status.UPLOADING);
+  }
+
+  /**
+   * Hide uploading preloader
+   *
+   * @returns {void}
+   */
+  hidePreloader() {
+    this.nodes.documentPreloader.style.backgroundImage = '';
+    this.toggleStatus(Ui.status.EMPTY);
+  }
+
+  /**
+   * Shows document
+   *
+   * @param {Object} file - document source
+   * @returns {void}
+   */
+  fillDocument(file) {
+    const attributes = {
+      src: "data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2232%22%20height%3D%2232%22%20fill%3D%22%235c6b7a%22%20viewBox%3D%220%200%20256%20256%22%3E%3Cpath%20d%3D%22M80%2C224a8%2C8%2C0%2C0%2C1-8%2C8H56a16%2C16%2C0%2C0%2C1-16-16V184a8%2C8%2C0%2C0%2C1%2C16%2C0v32H72A8%2C8%2C0%2C0%2C1%2C80%2C224ZM213.66%2C82.34l-56-56A8%2C8%2C0%2C0%2C0%2C152%2C24H120a8%2C8%2C0%2C0%2C0%2C0%2C16h24V88a8%2C8%2C0%2C0%2C0%2C8%2C8h48v40a8%2C8%2C0%2C0%2C0%2C16%2C0V88A8%2C8%2C0%2C0%2C0%2C213.66%2C82.34ZM80%2C24H56A16%2C16%2C0%2C0%2C0%2C40%2C40V64a8%2C8%2C0%2C0%2C0%2C16%2C0V40H80a8%2C8%2C0%2C0%2C0%2C0-16ZM208%2C168a8%2C8%2C0%2C0%2C0-8%2C8v40h-8a8%2C8%2C0%2C0%2C0%2C0%2C16h8a16%2C16%2C0%2C0%2C0%2C16-16V176A8%2C8%2C0%2C0%2C0%2C208%2C168ZM48%2C152a8%2C8%2C0%2C0%2C0%2C8-8V104a8%2C8%2C0%2C0%2C0-16%2C0v40A8%2C8%2C0%2C0%2C0%2C48%2C152Zm104%2C64H112a8%2C8%2C0%2C0%2C0%2C0%2C16h40a8%2C8%2C0%2C0%2C0%2C0-16Z%22%2F%3E%3C%2Fsvg%3E"
+    };
+
+    let eventName = 'load';
+
+    /**
+     * Compose tag with defined attributes
+     *
+     * @type {Element}
+     */
+    this.nodes.documentElement = make('IMG', this.CSS.documentElement, attributes);
+    this.nodes.documentTitle.innerHTML = file.title;
+    /**
+     * Add load event listener
+     */
+    this.nodes.documentElement.addEventListener(eventName, () => {
+      this.toggleStatus(Ui.status.FILLED);
+
+      /**
+       * Preloader does not exists on first rendering with presaved data
+       */
+      if (this.nodes.documentPreloader) {
+        this.nodes.documentPreloader.style.backgroundImage = '';
+      }
+    });
+    this.nodes.documentContainer.appendChild(this.nodes.documentElement);
+    this.nodes.documentContainer.appendChild(this.nodes.documentTitle);
+  }
+
+  /**
+   * Shows caption input
+   *
+   * @param {string} text - caption text
+   * @returns {void}
+   */
+  fillCaption(text) {
+    if (this.nodes.caption) {
+      this.nodes.caption.innerHTML = text;
+    }
+  }
+
+  /**
+   * Changes UI status
+   *
+   * @param {string} status - see {@link Ui.status} constants
+   * @returns {void}
+   */
+  toggleStatus(status) {
+    for (const statusType in Ui.status) {
+      if (Object.prototype.hasOwnProperty.call(Ui.status, statusType)) {
+        this.nodes.wrapper.classList.toggle(`${this.CSS.wrapper}--${Ui.status[statusType]}`, status === Ui.status[statusType]);
+      }
+    }
+  }
+
+  /**
+   * Apply visual representation of activated tune
+   *
+   * @param {string} tuneName - one of available tunes {@link Tunes.tunes}
+   * @param {boolean} status - true for enable, false for disable
+   * @returns {void}
+   */
+  applyTune(tuneName, status) {
+    this.nodes.wrapper.classList.toggle(`${this.CSS.wrapper}--${tuneName}`, status);
+  }
+}
